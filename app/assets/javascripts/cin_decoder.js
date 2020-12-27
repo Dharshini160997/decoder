1e91248d92d41d2090bb90ae2636ed936358024f
dharshini.s@caratlane.com <dharshini.s@caratlane.com>
2020-12-27 16:15:27 +0530

    const

--- app/assets/javascripts/cin_decoder.js
+++ app/assets/javascripts/cin_decoder.js
@@ -2,8 +2,8 @@ $(document).ready(function()
 {
   $(".decode-button").click(function(){
     var cin = $(".cin-input").val()
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
+    var queryString = window.location.search;
+    var urlParams = new URLSearchParams(queryString);
     var user_name  = urlParams.get('name');
 
 
@@ -46,8 +46,8 @@ $(document).ready(function()
   })
   $(".history-button").click(function(e){
     e.preventDefault()
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
+    var queryString = window.location.search;
+    var urlParams = new URLSearchParams(queryString);
     var user_name  = urlParams.get('name');
     window.location.href = "/user/history?user_name="+user_name
   })
